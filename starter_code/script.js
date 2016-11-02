ceea8f6108d1d82e4f3be802f778fc07f556ef7d
Lauren <lauren.sperber@gmail.com>
2016-11-02 20:17:38 +0000

    fix typos and convert divs to buttons

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,10 +3,10 @@ var generateComputerChoice = function () {
     // Task 1, Step 1: Create an array with three elements ("rock", "paper" and "scissors").
 
     // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
-    // number between 0 and 2.
+    // number between 0 and 2. Be sure to save it to a variable.
 
     // Task 1, Step 3: Use this randomly generated number to pull a value from the
-    // array (eg myArray[ranomNum])
+    // array (eg myArray[randomNum])
 
     // Task 1, Step 4: return this new value
 };
@@ -41,7 +41,7 @@ $(document).ready(function() {
     // Set `userChoice` to "null" and create a click handler that changes the
     // value based on the item the user clicks on the HTML page.
 
-    // This line callss the `pickWinner` function with the `userChoice` variable
+    // This line calls the `pickWinner` function with the `userChoice` variable
     // and the `computerChoice` variable.
     pickWinner(userChoice, computerChoice);
 
